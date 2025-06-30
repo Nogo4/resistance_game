843867dadcd7ad2e7deaad3693d22a5ca06445ee
Nogo4 <noah.gomes@epitech.eu>
2025-06-30 16:39:57 +0200

    feat(game): add rules and commands commands for better game guidance

--- src/utils/is_private_message.py
+++ src/utils/is_private_message.py
@@ -2,15 +2,9 @@ import discord
 
 async def is_private_message(message: discord.Message) -> bool:
     if isinstance(message.channel, discord.DMChannel):
-        if message.content == "!help":
-            await message.channel.send(
-                "This bot is in development, please wait for the next update."
-            )
-            return True
-        else:
-            await message.channel.send(
-                f"Hey <@{message.author.id}>, I'm a bot to play The Resistance Game.\n"
-                "Use the !help command in private messages to see how to play."
-            )
+        await message.channel.send(
+            f"Hey <@{message.author.id}>, I'm a bot to play The Resistance Game.\n"
+            "Use the /command command to see all commands available."
+        )
         return True
     return False
